b6e2531cad762e7527fc84d76f5ca1439a269d40
Nazarov Bahodur <nazarovbahodur1999@gmail.com>
2023-05-30 17:16:55 +0500

    set up secret key to .env file

--- wallet/auth.go
+++ wallet/auth.go
@@ -4,10 +4,12 @@ import (
 	"crypto/hmac"
 	"crypto/sha1"
 	"encoding/base64"
+	"os"
 )
 
 func AuthenticateRequest(body []byte, digest string) bool {
-	key := []byte("your-secret-key") // Replace with your secret key
+	secretKey := os.Getenv("SECRET_KEY")
+	key := []byte(secretKey)
 	hash := hmac.New(sha1.New, key)
 	hash.Write(body)
 	expectedDigest := base64.StdEncoding.EncodeToString(hash.Sum(nil))
