962cc1d5f45350d3481d120e965c2073333fec34
Nazarov Bahodur <nazarovbahodur1999@gmail.com>
2023-05-30 16:59:36 +0500

    new handler for Summary Tran

--- routes/route.go
+++ routes/route.go
@@ -13,6 +13,7 @@ func Listening() {
 	router.HandleFunc("/wallets", wallet.CreateWallet).Methods("POST")
 	router.HandleFunc("/wallets/{id}/exists", wallet.AccountExists).Methods("GET")
 	router.HandleFunc("/wallets/{id}/deposit", wallet.Deposit).Methods("POST")
-
+	router.HandleFunc("/wallets/{id}/summary", wallet.GetTransactionSummary).Methods("GET")
+	
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
