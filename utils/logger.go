3dd7838a9bada8251ae0ffb2291e6e310fd8c78b
Nazarov Bahodur <nazarovbahodur1999@gmail.com>
2023-05-30 13:12:08 +0500

    set up logger file

---
+++ utils/logger.go
@@ -0,0 +1,45 @@
+package utils
+
+import (
+	logg "log"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	Server *logg.Logger
+	Errl   *log.Logger
+
+	info *os.File
+	errl *os.File
+)
+
+func createFile(name string) *os.File {
+	file, err := os.OpenFile(name, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return file
+}
+
+func init() {
+	info = createFile("./logs/server.log")
+	errl = createFile("./logs/error.log")
+
+	Errl = log.New()
+	Server = logg.New(info, "INFO: ", logg.Ldate|logg.Ltime|logg.Lshortfile)
+
+	Errl.SetOutput(errl)
+}
+
+func CloseLogger() {
+	err := info.Close()
+	if err != nil {
+		log.Fatal("can't close file %v, %v", info, err)
+	}
+	err = errl.Close()
+	if err != nil {
+		log.Fatalf("can't close file %v, %v", errl, err)
+	}
+}
