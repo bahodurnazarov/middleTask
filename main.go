a2fa3fbbfba50aee32de366805a0cbc07a8e1991
Nazarov Bahodur <nazarovbahodur1999@gmail.com>
2023-05-30 13:16:13 +0500

    logger.go works well

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	lg "github.com/bahodurnazarov/middleTask/utils"
+)
 
 func main() {
 	fmt.Println("Hello")
+	lg.Errl.Println("Hello Error ")
+	lg.Server.Println("Server Hello")
+
 }
